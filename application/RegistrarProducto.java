7414ec8a8b02d24ce0f75b976592dc431f5bd930
Madakase <76799700+Madakase@users.noreply.github.com>
2026-03-04 13:02:14 -0500

    Added View module

---
+++ application/RegistrarProducto.java
@@ -0,0 +1,61 @@
+package application;
+
+import java.util.ArrayList;
+
+import database.ProductoDAO;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import model.Producto;
+
+public class RegistrarProducto extends Registrable{
+	
+	Producto producto;
+	ProductoDAO productoDB = new ProductoDAO();
+	
+	public void inicializarInterfazRegistro(VBox cajaCentro, String tipo) {
+		Button btnRegistrar = new Button("Registrar "+ tipo);
+		btnRegistrar.setOnAction(e-> ejecutarRegistroProducto());
+	
+		VBox cajaCampos = inicializarCajaCampos();
+		cajaCampos.setAlignment(Pos.CENTER);
+		
+		VBox cajaRegistro = new VBox(30);
+		cajaRegistro.setAlignment(Pos.CENTER);
+		cajaRegistro.getChildren().addAll(cajaCampos, btnRegistrar);
+		
+		cajaCentro.getChildren().add(cajaRegistro);
+	}
+	
+	public VBox inicializarCajaCampos(){
+		ArrayList<HBox> campos = new ArrayList<>();
+		
+		campos.add(this.crearCampoTexto("Nombre"));
+		campos.add(this.crearCampoTexto("Precio"));
+		campos.add(this.crearCampoTexto("Tamaño"));
+		
+		
+		VBox cajaDatos = new VBox(10);
+		cajaDatos.setAlignment(Pos.CENTER);
+		cajaDatos.getChildren().addAll(campos);
+		
+		return cajaDatos;
+		
+	}
+	
+	public void ejecutarRegistroProducto() {
+		
+		if (estanLlenos()) {
+			ArrayList<String> valores = getValores();
+			this.producto = new Producto(valores.get(0), Double.valueOf(valores.get(1)), Double.valueOf(valores.get(2)));
+			
+			productoDB.registrarProducto(producto);
+			
+			System.out.print("Producto registrado correctamente");
+		}else {
+			System.out.print("Existen campos obligatorios sin llenar");
+		}
+		
+	}
+}
